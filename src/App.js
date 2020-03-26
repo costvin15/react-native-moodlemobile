3b777460b8d649795c41da5f2c8629f75563c048
Vinicius Castro <costacastrovinicius7@gmail.com>
2020-03-26 19:24:45 -0300

    feature(loginservice): Renew token service implemented

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
-import {Home, Login} from './screens';
+import {Home, Login, Register} from './screens';
 
 const App = () => {
   const Stack = createStackNavigator();
 
   return (
     <NavigationContainer>
-      <Stack.Navigator headerMode={'none'} initialRouteName="Login">
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Login" component={Login} />
+      <Stack.Navigator headerMode={'none'} initialRouteName="login">
+        <Stack.Screen name="home" component={Home} />
+        <Stack.Screen name="login" component={Login} />
+        <Stack.Screen name="register" component={Register} />
       </Stack.Navigator>
     </NavigationContainer>
   );
