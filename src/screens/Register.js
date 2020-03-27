040b8c2d3d015b1109cf0d0c959cbd532c8bb103
Vinicius Castro <costacastrovinicius7@gmail.com>
2020-03-27 01:08:52 -0300

    feature(loginservice): Calling email signup moodle webservice

--- src/screens/Register.js
+++ src/screens/Register.js
@@ -1,8 +1,11 @@
-import React, {useState, useEffect} from 'react';
-import {StyleSheet, SafeAreaView, Text, TextInput} from 'react-native';
+import React, {useState, useEffect, createRef} from 'react';
+import {StyleSheet, SafeAreaView, Text, TextInput, Button} from 'react-native';
 import {callMoodleWebService} from '../api/helper';
 
 const Register = () => {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [fields, setFields] = useState([]);
   const [passwordpolicy, setPasswordpolicy] = useState('');
 
@@ -25,11 +28,29 @@ const Register = () => {
         const placeholder = await callMoodleWebService('core_get_string', {
           stringid: field,
         });
-        fieldList.push({stringid: field, placeholder});
+        fieldList.push({stringid: field, placeholder, ref: createRef()});
       }
 
       setFields(fieldList);
       setPasswordpolicy(passwordpolicy);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const makeRegister = async () => {
+    const values = {};
+    fields.map(value => {
+      values[value.stringid] = value.ref.current._lastNativeText;
+    });
+    try {
+      const response = await callMoodleWebService('auth_email_signup_user', {
+        username,
+        password,
+        email,
+        ...values,
+      });
+      console.log(response);
     } catch (error) {
       console.error(error);
     }
@@ -41,10 +62,11 @@ const Register = () => {
 
   return (
     <SafeAreaView>
-      {fields.map(({stringid, placeholder}) => {
+      {fields.map(({stringid, placeholder, ref}) => {
         return (
           <TextInput
             key={stringid}
+            ref={ref}
             placeholder={placeholder}
             style={styles.input}
             placeholderTextColor={'#000'}
@@ -52,16 +74,25 @@ const Register = () => {
         );
       })}
       <TextInput
+        placeholder={'Nome de usuario'}
+        style={styles.input}
+        placeholderTextColor={'#000'}
+        onChangeText={text => setUsername(text)}
+      />
+      <TextInput
         placeholder={'Email'}
         style={styles.input}
         placeholderTextColor={'#000'}
+        onChangeText={text => setEmail(text)}
       />
       <TextInput
         placeholder={'Senha'}
         style={styles.input}
         placeholderTextColor={'#000'}
+        onChangeText={text => setPassword(text)}
       />
       <Text>{passwordpolicy}</Text>
+      <Button onPress={makeRegister} title={'Registrar'} />
     </SafeAreaView>
   );
 };
