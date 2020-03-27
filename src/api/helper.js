47b8b270e4ff6b28216d286ac1ec8902540140c0
Vinicius Castro <costacastrovinicius7@gmail.com>
2020-03-26 21:07:06 -0300

    feature(loginservice): Signup settings webservice consumer implemented

--- src/api/helper.js
+++ src/api/helper.js
@@ -1,13 +1,19 @@
 import AsyncStorage from '@react-native-community/async-storage';
 import Constants from './constants';
 
-export const callMoodleWebService = async (wsfunction, {...params}) => {
+export const callMoodleWebService = async (wsfunction, ...params) => {
   const token = await AsyncStorage.getItem(Constants.MOODLE_USER_TOKEN);
-  const response = await fetch(
-    `${
-      Constants.MOODLE_HOST
-    }/webservice/rest/server.php?moodlewsrestformat=json&wstoken=${token}&wsfunction=${wsfunction}`,
-  );
+  var url = `${
+    Constants.MOODLE_HOST
+  }/webservice/rest/server.php?moodlewsrestformat=json&wstoken=${token}&wsfunction=${wsfunction}`;
+
+  params.forEach(param => {
+    Object.keys(param).forEach(key => {
+      url += `&${key}=${param[key]}`;
+    });
+  });
+
+  const response = await fetch(url);
 
   const data = await response.json();
 
@@ -20,9 +26,9 @@ export const callMoodleWebService = async (wsfunction, {...params}) => {
 
 export const renewMoodleUserToken = async ({username, password}) => {
   const response = await fetch(
-    `${
-      Constants.MOODLE_HOST
-    }/login/token.php?service=moodle_mobile_app&username=${username}&password=${password}`,
+    `${Constants.MOODLE_HOST}/login/token.php?service=${
+      Constants.MOODLE_SERVICE
+    }&username=${username}&password=${password}`,
   );
   const data = await response.json();
 
--- src/screens/Register.js
+++ src/screens/Register.js
@@ -1,10 +1,67 @@
-import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {StyleSheet, SafeAreaView, Text, TextInput} from 'react-native';
+import {callMoodleWebService} from '../api/helper';
 
 const Register = () => {
+  const [fields, setFields] = useState([]);
+  const [passwordpolicy, setPasswordpolicy] = useState('');
+
+  const styles = StyleSheet.create({
+    input: {
+      height: 50,
+      borderColor: '#000',
+      borderWidth: 1,
+      color: '#000',
+    },
+  });
+
+  const getSignupSettings = async () => {
+    try {
+      const {namefields, passwordpolicy, warnings} = await callMoodleWebService(
+        'auth_email_get_signup_settings',
+      );
+      const fieldList = [];
+      for (const field of namefields) {
+        const placeholder = await callMoodleWebService('core_get_string', {
+          stringid: field,
+        });
+        fieldList.push({stringid: field, placeholder});
+      }
+
+      setFields(fieldList);
+      setPasswordpolicy(passwordpolicy);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getSignupSettings();
+  }, []);
+
   return (
     <SafeAreaView>
-      <Text>Register</Text>
+      {fields.map(({stringid, placeholder}) => {
+        return (
+          <TextInput
+            key={stringid}
+            placeholder={placeholder}
+            style={styles.input}
+            placeholderTextColor={'#000'}
+          />
+        );
+      })}
+      <TextInput
+        placeholder={'Email'}
+        style={styles.input}
+        placeholderTextColor={'#000'}
+      />
+      <TextInput
+        placeholder={'Senha'}
+        style={styles.input}
+        placeholderTextColor={'#000'}
+      />
+      <Text>{passwordpolicy}</Text>
     </SafeAreaView>
   );
 };
