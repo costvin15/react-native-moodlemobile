37e6b8c8275ff375db8d711e0e49cf789fd88bf6
Vinicius Castro <costacastrovinicius7@gmail.com>
2020-03-28 18:44:23 -0300

    feature(tabs/about): Adding about tab on navigator

--- src/screens/DashboardContext/index.js
+++ src/screens/DashboardContext/index.js
@@ -3,6 +3,7 @@ import {createMaterialBottomTabNavigator} from '@react-navigation/material-botto
 
 import Dashboard from './Dashboard';
 import Messages from './Messages';
+import About from './About';
 
 const DashboardContext = () => {
   const Tab = createMaterialBottomTabNavigator();
@@ -11,6 +12,7 @@ const DashboardContext = () => {
     <Tab.Navigator initialRouteName="dashboard">
       <Tab.Screen name="dashboard" component={Dashboard} />
       <Tab.Screen name="messages" component={Messages} />
+      <Tab.Screen name="about" component={About} />
     </Tab.Navigator>
   );
 };
