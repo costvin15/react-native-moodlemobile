97c17aba9952204255a0b3c1675f8ed3406fcc35
Vinicius Castro <costacastrovinicius7@gmail.com>
2020-03-28 23:51:19 -0300

    fix(tabs/message): Message tab view fixed

--- src/screens/DashboardContext/Messages/index.js
+++ src/screens/DashboardContext/Messages/index.js
@@ -12,10 +12,11 @@ const Dashboard = ({navigation, route}) => {
   return (
     <SafeAreaView>
       <Text>Messages</Text>
-      {conversations.map(({conversation, index}) => {
+      {conversations.map((conversation, index) => {
+        console.log(conversation);
         return (
           <View key={index}>
-            {conversation.members.map(member => (
+            {conversation?.members.map(member => (
               <Text>{member.fullname}</Text>
             ))}
             {conversation.messages.map(message => (
