5a718ab6c0185eac045ed62497bc0ed4e3418043
Vinicius Castro <costacastrovinicius7@gmail.com>
2020-03-30 03:50:59 -0300

    feature(blocks/recentlyaccessedcourses): Course list provider and view implemented

---
+++ src/blocks/recentlyaccessedcourses/styles.js
@@ -0,0 +1,12 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  coursesContainer: {
+    margin: 15,
+  },
+  courseImage: {
+    height: 170,
+  },
+});
+
+export default styles;
