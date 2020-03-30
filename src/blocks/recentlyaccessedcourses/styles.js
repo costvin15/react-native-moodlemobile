dd2eeaca20882835e425c75335346a27a2d76641
Vinicius Castro <costacastrovinicius7@gmail.com>
2020-03-30 04:08:45 -0300

    feature(blocks/recentlyaccessedcourses): Block view improved

--- src/blocks/recentlyaccessedcourses/styles.js
+++ src/blocks/recentlyaccessedcourses/styles.js
@@ -4,8 +4,26 @@ export const styles = StyleSheet.create({
   coursesContainer: {
     margin: 15,
   },
+  courseContainer: {
+    width: 300,
+  },
+  courseItemMargin: {
+    marginRight: 15,
+  },
   courseImage: {
     height: 170,
+  },
+  courseNoImage: {
+    backgroundColor: '#cacaca',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  courseFooter: {
+    padding: 10,
+    backgroundColor: '#f1f1f1',
+  },
+  courseTitle: {
+    fontSize: 16,
   },
 });
 
