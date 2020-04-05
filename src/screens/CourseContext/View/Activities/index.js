e9e16dfd7de2a2270b00935378d915d28acc0acd
Vinicius Castro <costacastrovinicius7@gmail.com>
2020-04-05 00:54:06 -0300

    feature(course/view): Activities tab view improved

--- src/screens/CourseContext/View/Activities/index.js
+++ src/screens/CourseContext/View/Activities/index.js
@@ -1,7 +1,50 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {View, FlatList, Text} from 'react-native';
+import Provider from './provider';
+import Accordion from 'react-native-collapsible/Accordion';
 
-const Activities = () => {
-  return <></>;
+const Activities = ({route}) => {
+  const [sections, setSections] = useState([]);
+  const [activeSections, setActiveSections] = useState([0]);
+
+  useEffect(() => {
+    Provider.getSectionAndActivities(route.params.id).then(data =>
+      setSections(data),
+    );
+  }, [route.params.id]);
+
+  const _renderHeader = section => {
+    return (
+      <View>
+        <Text>{section.name}</Text>
+      </View>
+    );
+  };
+
+  const _renderContent = section => {
+    return (
+      <View>
+        <Text>Content</Text>
+      </View>
+    );
+  };
+
+  const _updateSections = actives => {
+    setActiveSections(actives);
+  };
+
+  return (
+    <View>
+      <Accordion
+        sections={sections}
+        activeSections={activeSections}
+        renderSectionTitle={() => <></>}
+        renderHeader={_renderHeader}
+        renderContent={_renderContent}
+        onChange={_updateSections}
+      />
+    </View>
+  );
 };
 
 export default Activities;
