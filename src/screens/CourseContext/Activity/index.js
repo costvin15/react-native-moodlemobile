b4f55cc8e079793749a7729bcce6b3d89962be40
Vinicius Castro <costacastrovinicius7@gmail.com>
2020-04-15 00:31:02 -0300

    feature(blocks/recentlyaccessedcourses): Emmiting event activity

---
+++ src/screens/CourseContext/Activity/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Activity = () => {
+  return (
+    <View>
+      <Text>Hello, Activity</Text>
+    </View>
+  );
+};
+
+export default Activity;
--- src/screens/CourseContext/index.js
+++ src/screens/CourseContext/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 
 import View from './View';
+import Activity from './Activity';
 
 const CourseContext = () => {
   const Stack = createStackNavigator();
 
   return (
     <Stack.Navigator headerMode="none">
+      <Stack.Screen name="activity" component={Activity} />
       <Stack.Screen name="view" component={View} />
     </Stack.Navigator>
   );
