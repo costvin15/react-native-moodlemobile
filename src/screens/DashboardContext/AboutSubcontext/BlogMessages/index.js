38dcff15696ffda27c348369aa857fadd1442253
Vinicius Castro <costacastrovinicius7@gmail.com>
2020-04-17 19:54:28 -0300

    feature(about/blogmessages): Provider implemented

--- src/screens/DashboardContext/AboutSubcontext/BlogMessages/index.js
+++ src/screens/DashboardContext/AboutSubcontext/BlogMessages/index.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {Page} from '../../../../components';
+import Provider from './provider';
+import {View, Text} from 'react-native';
 
-const BlogMessages = () => {
+const BlogMessages = ({navigation, route}) => {
+  const [entries, setEntries] = useState([]);
+
+  useEffect(() => {
+    Provider.getEntries({id: 221}).then(data => setEntries(data));
+  }, [route.params.id]);
+
   return (
-    <Page appbar={{
-      title: 'Mensagens do blog'
-    }} />
+    <Page
+      appbar={{
+        title: 'Mensagens do blog',
+        canGoBack: navigation.canGoBack(),
+        goBack: navigation.goBack,
+      }}>
+      <View>
+        {entries.map(entry => {
+          console.log(entry);
+          return (
+            <View>
+              <Text>{entry.subject}</Text>
+            </View>
+          );
+        })}
+      </View>
+    </Page>
   );
 };
 
