d0091761807c0f2886ad70b627235d8a857b01b4
Vinicius Castro <costacastrovinicius7@gmail.com>
2020-04-17 19:18:37 -0300

    feature(about/blogmessages): BlogMessages event created

---
+++ src/screens/DashboardContext/AboutSubcontext/BlogMessages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Page} from '../../../../components';
+
+const BlogMessages = () => {
+  return (
+    <Page appbar={{
+      title: 'Mensagens do blog'
+    }} />
+  );
+};
+
+export default BlogMessages;
