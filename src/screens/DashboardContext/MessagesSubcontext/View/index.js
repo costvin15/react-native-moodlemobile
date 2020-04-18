54745cd5e54576d2bdc7ed5f313efdb01fa73e17
Vinicius Castro <costacastrovinicius7@gmail.com>
2020-04-18 08:47:29 -0300

    feature(tabs/message): Chat message improved

--- src/screens/DashboardContext/MessagesSubcontext/View/index.js
+++ src/screens/DashboardContext/MessagesSubcontext/View/index.js
@@ -5,31 +5,44 @@ import {GiftedChat} from 'react-native-gifted-chat';
 import {View, Text} from 'react-native';
 
 const ConversationView = ({navigation}) => {
-  const [conversation, setConversation] = useState({});
+  const [title, setTitle] = useState('');
+  const [members, setMembers] = useState([]);
+  const [messages, setMessages] = useState([]);
 
-  const messages = [
-    {
-      _id: 1,
-      text: 'Hello developer',
-      createdAt: new Date(),
-      user: {
-        _id: 2,
-        name: 'React Native',
-        avatar: 'https://placeimg.com/140/140/any',
-      },
-    },
-  ];
+  // const messages = [
+  //   {
+  //     _id: 1,
+  //     text: 'Hello developer',
+  //     createdAt: new Date(),
+  //     user: {
+  //       _id: 2,
+  //       name: 'React Native',
+  //       avatar: 'https://placeimg.com/140/140/any',
+  //     },
+  //   },
+  // ];
 
   useEffect(() => {
     Provider.getConversationMessages().then(data => {
-      setConversation(data);
+      setTitle(data.name);
+
+      const resultMessages = [];
+      data.messages.forEach(({text, timecreated}, index) => {
+        resultMessages.push({
+          _id: index,
+          text,
+          createdAt: new Date(timecreated * 1000),
+          user: {_id: 2, name: 'ReactNative'},
+        });
+      });
+      setMessages(resultMessages);
     });
   }, []);
 
   return (
     <Page
       appbar={{
-        title: conversation?.name,
+        title: title,
         canGoBack: navigation.canGoBack(),
         goBack: navigation.goBack,
         hasScrolView: false,
