60b1a1b0ef5d4e9273db0f4255f99d4175cdee5e
Vinicius Castro <costacastrovinicius7@gmail.com>
2020-04-18 01:53:58 -0300

    feature(tabs/message): Message tab view improved

---
+++ src/screens/DashboardContext/Messages/styles.js
@@ -0,0 +1,17 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  headerUnactive: {
+    borderRadius: 0,
+    borderBottomWidth: 0,
+    borderTopWidth: 0,
+    borderBottomColor: '#fff',
+    borderTopColor: '#fff',
+  },
+  headerActive: {
+    marginTop: 15,
+    marginHorizontal: 15,
+  },
+});
+
+export default styles;
