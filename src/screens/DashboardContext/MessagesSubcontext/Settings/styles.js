d879c50d92cb6633e0cd456e35c8a6de5816a50c
Vinicius Castro <costacastrovinicius7@gmail.com>
2020-04-23 23:31:06 -0300

    feature(message/configurations): Message configuration page improved

---
+++ src/screens/DashboardContext/MessagesSubcontext/Settings/styles.js
@@ -0,0 +1,21 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  marginVerticalDefault: {
+    marginVertical: 15,
+  },
+  marginHorizontalDefault: {
+    marginHorizontal: 15,
+  },
+  rowDirection: {
+    flexDirection: 'row',
+  },
+  spaceBetween: {
+    justifyContent: 'space-between',
+  },
+  alignCenter: {
+    alignItems: 'center',
+  },
+});
+
+export default styles;
