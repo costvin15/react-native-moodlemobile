855ccddd5ccefa3756faf1827479935f7fa75e17
Vinicius Castro <costacastrovinicius7@gmail.com>
2020-04-27 20:11:21 -0300

    task(internationalization): Course pages strings moved to i18n

--- src/screens/CourseContext/View/index.js
+++ src/screens/CourseContext/View/index.js
@@ -8,6 +8,7 @@ import Provider from './provider';
 import Activities from './Activities';
 import Participants from './Participants';
 import {useTheme} from 'react-native-paper';
+import Locales from '../../../locales';
 
 const Course = ({navigation, route}) => {
   const [course, setCourse] = useState({});
@@ -38,12 +39,12 @@ const Course = ({navigation, route}) => {
       headerMode="none"
       initialRouteName="view">
       <Tab.Screen
-        name="activities"
+        name={Locales.t('activities')}
         component={Activities}
         initialParams={route.params}
       />
       <Tab.Screen
-        name="participants"
+        name={Locales.t('participants')}
         component={Participants}
         initialParams={route.params}
       />
