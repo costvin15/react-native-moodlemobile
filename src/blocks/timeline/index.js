710096d91d97e6750ce3761945e0e30c83a16be9
Vinicius Castro <costacastrovinicius7@gmail.com>
2020-04-28 01:50:01 -0300

    feature(blocks/timeline): Timeline block refactored

--- src/blocks/timeline/index.js
+++ src/blocks/timeline/index.js
@@ -1,40 +1,52 @@
 import React, {useState, useEffect} from 'react';
 import {View, Text} from 'react-native';
-// TODO: Replace material-ui with paper
-import {Subheader, Card, Divider} from 'react-native-material-ui';
-import Provider from './provider';
-import Styles from './styles';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import {Card, Divider} from 'react-native-paper';
+
+import Provider from './provider';
+import {styles} from './styles';
 
 const Timeline = () => {
+  const [isLoading, setIsLoading] = useState(true);
   const [events, setEvents] = useState([]);
 
   useEffect(() => {
     Provider.getActionsEventsByTimesort().then(data => setEvents(data.events));
+    setIsLoading(false);
   }, []);
 
   return (
     <View>
-      <Card>
-        <Subheader text="Timeline" />
-        <Divider />
+      <Card
+        style={{
+          ...styles.marginVerticalDefault,
+          ...styles.marginHorizontalDefault,
+        }}>
+        <Card.Title title="Timeline" />
 
-        {events.map(event => (
-          <View style={Styles.eventContainer} key={event.id}>
-            <Text style={Styles.eventTitle}>{event.name}</Text>
-            <Text style={Styles.eventCourseTitle}>{event.course.fullname}</Text>
-            <Text>
-              {new Date(event.timestart * 1000).toLocaleString('pt-BR')}
-            </Text>
-            <Divider style={{container: Styles.eventDivider}} />
-          </View>
-        ))}
+        {(isLoading && <></>) || (
+          <>
+            {/* TODO: Replace with listview */}
+            {events.map(event => (
+              <View key={event.id} style={{...styles.marginHorizontalDefault}}>
+                <Text style={styles.eventTitle}>{event.name}</Text>
+                <Text style={styles.eventCourseTitle}>
+                  {event.course.fullname}
+                </Text>
+                <Text>
+                  {new Date(event.timestart * 1000).toLocaleString('pt-BR')}
+                </Text>
+                <Divider style={{...styles.marginVerticalDefault}} />
+              </View>
+            ))}
 
-        {events.length === 0 && (
-          <View style={Styles.noEventsContainer}>
-            <MaterialIcons name="event-busy" size={40} />
-            <Text>Você não tem tarefas</Text>
-          </View>
+            {events.length === 0 && (
+              <View style={styles.noEventsContainer}>
+                <MaterialIcons name="event-busy" size={40} />
+                <Text>Você não tem tarefas</Text>
+              </View>
+            )}
+          </>
         )}
       </Card>
     </View>
