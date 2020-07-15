da45ed34135c83a2029469e838b47ea0df56bb92
Vinicius Castro <costacastrovinicius7@gmail.com>
2020-07-14 22:34:03 -0300

    feature(about/details): Logging user profile viewed on moodle
    Logger moved to events

--- src/screens/CourseContext/View/index.js
+++ src/screens/CourseContext/View/index.js
@@ -19,7 +19,6 @@ const Course = ({navigation, route}) => {
 
   useEffect(() => {
     Provider.getCourseDetail(route.params.id).then(data => setCourse(data));
-    Provider.logCourseWasViewed(route.params.id);
   }, [route.params.id]);
 
   return (
